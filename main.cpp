5d871daa2935a5ac35c5bec5c043253c5473ea7c
Jasin Atipi <jasin.atipi@icare-world.com>
2024-06-11 21:49:22 +0200

    first commit
    Introduce main.cpp Hello World, CMakeLists.txt, .gitignore

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
