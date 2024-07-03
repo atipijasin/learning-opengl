eab03646eb34cd12477adb517f47d77f7cefefbb
Jasin <jasin@atipi.it>
2024-07-03 23:48:30 +0200

    draw a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -86,12 +86,6 @@ int main() {
 
     float vertices[] = {x1, y1, z1, x2, y2, z2, x3, y3, z3};
 
-    // Vertex Buffer Objects
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -136,11 +130,19 @@ int main() {
                   << infoLog << std::endl;
     }
 
+    unsigned int VAO; // Vertex Array Objects
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    unsigned int VBO; // Vertex Buffer Objects
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
     glUseProgram(shaderProgram);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-
-    // TODO link vertex attributes (5.5)
 
     // Render loop
     while (!glfwWindowShouldClose(window)) {
@@ -149,6 +151,8 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
